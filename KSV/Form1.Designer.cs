2450b07a9f92a4068873017fcc4cbf45859064f5
Kasyanenko.ID <lazy-777@yandex.ru>
2016-01-10 15:25:04 +0500

    Создан массив с фразами и появление их в рандоме Изменено начальное положение. Сначала появляется по центру, потом перемещается по таймеру по указанным координатам

--- KSV/Form1.Designer.cs
+++ KSV/Form1.Designer.cs
@@ -31,9 +31,9 @@
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.timerClosing = new System.Windows.Forms.Timer(this.components);
             this.timerTextColor = new System.Windows.Forms.Timer(this.components);
+            this.timerPosition = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // timer1
@@ -50,15 +50,6 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 239);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
-            // 
             // timerClosing
             // 
             this.timerClosing.Tick += new System.EventHandler(this.timerClosing_Tick);
@@ -67,14 +58,20 @@
             // 
             this.timerTextColor.Tick += new System.EventHandler(this.timerTextColor_Tick);
             // 
+            // timerPosition
+            // 
+            this.timerPosition.Interval = 3000;
+            this.timerPosition.Tick += new System.EventHandler(this.timerPosition_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
             this.Text = "Form1";
             this.TopMost = true;
             this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
@@ -91,8 +88,8 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Timer timerClosing;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Timer timerTextColor;
+        private System.Windows.Forms.Timer timerPosition;
     }
 }
 
--- KSV/Form1.cs
+++ KSV/Form1.cs
@@ -8,35 +8,56 @@ namespace KSV
     {
         private int SizeMonitorHeight = 0;      //Вертикаль X
         private int SizeMonitorWidth = 0;       //Горизонталь Y
+        string[] MsText = new string[20];
+        private Random rnd = new Random();
 
         public Form1()
         {
             InitializeComponent();
 
+            MsText[0] = "Ту-ру-ту-тут-ту-ру! Ксения, мне кажется, настало время поливки цветов!";
+            MsText[1] = "Цветы сами не польются!\rСрочно бросай все и полей цветы";
+            MsText[2] = "Если польешь все цветочки, то будет тебе счастье";
+            MsText[3] = "Нельзя не полить цветочки! Они могут погибнуть и приходить тебе в страшных снах";
+            MsText[4] = "Вчера ты отдыхала, сегодня настало время полить цветы";
+            MsText[5] = "Великие дела подождут, а пока полей цветочки";
+            MsText[6] = "Дисциплина — это решение делать то, чего очень не хочется делать, например полить цветы5";
+            MsText[7] = "В жизни каждого человека есть два самых важных дня: первый — это когда он родился, а второй — когда нужно полить цветы";
+            MsText[8] = "Одержать сто побед в ста битвах - это не подвиг! подвиг - встать и полить цветы";
+            MsText[9] = "ильный человек - это не тот, у которого все хорошо, а тот кто встал и полил цветы";
+
+            //Form1
             this.ShowInTaskbar = false;     //Убираем значек из панели задач
             this.FormBorderStyle = FormBorderStyle.None;        //форма без стиля
             SizeMonitorHeight = SystemInformation.PrimaryMonitorSize.Height - 250;
             SizeMonitorWidth = SystemInformation.PrimaryMonitorSize.Width - 250;
+            this.Size = new Size(270, 200);
+            this.StartPosition = FormStartPosition.CenterScreen;        //Начальное положение формы на экране
 
             //Устанавливаем интервалы таймеров
-            timer1.Interval = 230;
-            timerClosing.Interval = 5000;
+            timer1.Interval = 130;
+            timerClosing.Interval = 2000;
             timerTextColor.Interval = 250;
+            timerPosition.Interval = 2000;
 
-            label1.Font = new Font(label1.Font.Name, 15, FontStyle.Bold);   //Стиль текста
-            label1.MaximumSize = new Size(200,200);     //Размер Label для переноса строк
-            label1.Text = "Какой-то очень важный текст!";
-
-            this.Size = new Size(250, 250);
+            //label1
+            label1.Font = new Font(label1.Font.Name, 12, FontStyle.Bold);   //Стиль текста
+            label1.MaximumSize = new Size(270, 180);     //Размер Label для переноса строк
+            label1.Text = "L1";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            //label1.AutoSize = false;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.Location = new Point((int)SizeMonitorWidth / 100 * 90, (int)SizeMonitorHeight / 100 * 5);       //Установка положения формы на экране (X,Y)
             timer1.Start();             //Таймер для смены цвета фона
             timerClosing.Start();       //Таймер закрытия окна
             timerTextColor.Start();     //Таймер для смены цвета текста
-            System.Diagnostics.Process.Start("http://ya.ru");
+            timerPosition.Start();      //Таймер для смены положения
+            //this.Opacity = .100;
+            //System.Diagnostics.Process.Start("http://ya.ru");     //Переход по ссылке
+            //label1.Text = MsText[rnd.Next(0, 9)];
+            label1.Text = MsText[7];
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -85,6 +106,16 @@ namespace KSV
         {
             //Закрытие приложения
             Application.Exit();
+        }
+
+        private void timerPosition_Tick(object sender, EventArgs e)
+        {
+            //Положение формы по истечению таймера
+            this.Location = new Point((int)SizeMonitorWidth / 100 * 90, (int)SizeMonitorHeight / 100 * 5);       //Установка положения формы на экране (X,Y)
+            //изменение времени таймера цвета фона
+            timer1.Interval = 230;
+            //Изменение прозрачности фона
+            this.Opacity = .97;
         }
     }
 }
